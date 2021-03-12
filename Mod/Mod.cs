25e9f661205476cabae04dfad72cdb3a8e08ee19
Captain McScroggo <shutupyoufool@gmail.com>
2021-03-12 00:15:49 -0500

    Add project files.

---
+++ Mod/Mod.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System.Linq;
+
+namespace EnemySense {
+	[BepInPlugin("org.kodaichizero.enemysense", "EnemySense", "1.0.0.0")]
+	public class Mod : BaseUnityPlugin {
+		private static readonly Harmony harmony = new(typeof(Mod).GetCustomAttributes(typeof(BepInPlugin), false)
+			.Cast<BepInPlugin>()
+			.First()
+			.GUID);
+
+		private void Awake() {
+			harmony.PatchAll();
+		}
+
+		private void OnDestroy() {
+			harmony.UnpatchSelf();
+		}
+	}
+}
